3b34de24f8340886e89898127ea124ce90711815
georgfalemark <georg@falemark.se>
2018-12-04 11:43:04 +0100

    module9

---
+++ C#/Module9/Module9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Module9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var watcher = new FileSystemWatcher();
+            watcher.Path = @"C:\TMP";
+            watcher.EnableRaisingEvents = true;
+
+            watcher.Created += FileCreated;
+            watcher.Deleted += FileDelete;
+            watcher.Renamed += FileRename;
+
+            Console.ReadKey();
+
+        }
+
+        private static void FileRename(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"File {e.Name} was renamed");
+        }
+
+        private static void FileDelete(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File {e.Name} was removed");
+        }
+
+        private static void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File {e.Name} was created");
+        }
+    }
+}
