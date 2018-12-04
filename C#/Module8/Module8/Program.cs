0b6e458ef3fe54f6b103eb81627dc81367361dfe
georgfalemark <georg@falemark.se>
2018-12-04 10:47:12 +0100

    uppdatering module 8

--- C#/Module8/Module8/Program.cs
+++ C#/Module8/Module8/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Module8
 {
@@ -12,7 +13,88 @@ namespace Module8
 
             //FileException();
 
+
+            Run();
+
+
         }
+
+
+
+
+
+        private static void Run()
+        {
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Enter a list of animals: ");
+
+                try
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    string animal = Console.ReadLine();
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    var animallist = ParseAnimals(animal);
+                    Console.WriteLine($"There are {animallist.Length} animals in the list");
+                    return;
+
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static string[] ParseAnimals(string animalsString)
+        {
+            string[] animallist = animalsString.Split(',');
+
+            //IF satser, kommer kopplas tillbaka
+
+            if (string.IsNullOrWhiteSpace(animalsString))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                throw new ArgumentException("The animal list don't contain any letters");
+            }
+
+
+            foreach (string animal in animallist)
+            {
+                if (animal.Length > 20)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    throw new ArgumentException($"The animal: {animal} has to many letters");
+                }
+                if (animal.Length == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    throw new ArgumentException($"One of the animals didn't contain any letter");
+                }
+                if (!Regex.IsMatch(animal, @"^[a-zA-ZåäöÅÄÖ]+$"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    throw new ArgumentException($"Animal: {animal} contains invalid letters");
+                }
+            }     
+            return animallist;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 
         private static void ChocolateException()
         {
