5a496c82b41304d318c29e173f068fc6fe255988
georgfalemark <georg@falemark.se>
2018-12-11 14:17:25 +0100

    Module11

--- C#/Module11/Module11/Program.cs
+++ C#/Module11/Module11/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Module11
@@ -8,59 +9,165 @@ namespace Module11
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine();
+
+            //var Masterlist = new List<double> { 33, 54, 15 };
+
+            ////Här förstår de "Masterlist" eftersom det är inom samma klamrar
+            //double mySum = Sum(Masterlist);
+            //Console.WriteLine($"Summan av talen UTAN LINK är {mySum}");
+
+            //double myAverage = Average(Masterlist);
+            //Console.WriteLine($"Medelvärdet av talen UTAN LINK är {myAverage}");
+
+            //double myLinqSum = Sum_WithLinq(Masterlist);
+            ////SKRIV UT HÄR
+
+            //double myLinqAverage = Average_WithLinq(Masterlist);
+            ////SKRIV UT HÄR
+
+            //List <double> myNumbersHigherThanFive = NumbersHigherThanFive(Masterlist);
+            //Console.WriteLine($"Nummer högre än {myNumbersHigherThanFive}");
+
+            //// StariFyList(Masterlist);
 
 
 
-            Console.Write("Kalkylränta: ");
-            double r = Convert.ToInt32(Console.ReadLine());
+            List<Show> allShows = GetShowsFromTextFile();
 
-            Console.Write("År: ");
-            double n = Convert.ToInt32(Console.ReadLine());
+            DisplayInfoAboutShows(allShows);
 
-            Console.Write("Grundinvestering: ");
-            double G = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Inbetalningsöverskott: ");
-            double a = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
 
 
-            double r_n = Math.Pow(r, n);      
-            double NPV = -G + a / r_n;
-
-            Console.WriteLine($"NPV: {NPV}");
-
-
-
-
-
-
-
-
-            Console.WriteLine();
-
-            var Masterlist = new List<double> { 33, 54, 15 };
-
-            //Här förstår de "Masterlist" eftersom det är inom samma klamrar
-            double mySum = Sum(Masterlist);
-            Console.WriteLine($"Summan av talen UTAN LINK är {mySum}");
-
-            double myAverage = Average(Masterlist);
-            Console.WriteLine($"Medelvärdet av talen UTAN LINK är {myAverage}");
-
-            double myLinqSum = Sum_WithLinq(Masterlist);
-            //SKRIV UT HÄR
-
-            double myLinqAverage = Average_WithLinq(Masterlist);
-            //SKRIV UT HÄR
-
-            List <double> myNumbersHigherThanFive = NumbersHigherThanFive(Masterlist);
-            Console.WriteLine($"Nummer högre än {myNumbersHigherThanFive}");
-
-
-
-            // StariFyList(Masterlist);
 
         }
+
+        private static List<Show> GetShowsFromTextFile()
+        {
+            List<Show> Shower = new List<Show>();
+            string[] shower = File.ReadAllLines(@"C:\TMP\tv-data.txt");
+
+            foreach (string item in shower)
+            {
+                string[] deladeShower = item.Split("*");
+
+                var s = new Show();
+                s.Channel = deladeShower[0];
+                TimeSpan time = TimeSpan.Parse(deladeShower[1]);
+                s.StartTime = time;
+                s.Title = deladeShower[2];
+
+                Shower.Add(s);
+            }
+            return Shower;
+        }
+
+        private static void DisplayInfoAboutShows(List<Show> allShows)
+        {
+            Console.WriteLine("ALLA TITLAR");
+            Console.WriteLine();
+
+            foreach (Show show in allShows)
+            {
+                Console.WriteLine(show.Title);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PROGRAM SOM BÖRJAR SENARE ÄN KL 21");
+            Console.WriteLine();
+
+            foreach (Show show in allShows)
+            {
+                if (show.StartTime.Hours >= 21)
+                {
+                    Console.WriteLine($"{show.Channel} {show.StartTime} {show.Title}");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PROGRAM FRÅN SVT2 I KRONOLOGISK ORDNING");
+            Console.WriteLine();
+
+            foreach (Show show in allShows)
+            {
+                if (show.Channel == "SVT2")
+                {
+                    Console.WriteLine($"{show.Channel} {show.StartTime} {show.Title}");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("FINNS PROGRAMMET 'KULTURNYHETERNA'? ");
+            Console.WriteLine();
+
+            foreach (Show show in allShows)
+            {
+                if (show.Title == "Kulturnyheterna")
+                {
+                    Console.WriteLine("Ja");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("FINNS PROGRAMMET 'ENSAM PAPPA SÖKER'? ");
+            Console.WriteLine();
+
+            int counter = 0;
+            foreach (Show show in allShows)
+            {
+                if (show.Title != "Ensam pappa söker" && counter == 0)
+                {
+                    Console.WriteLine("Nej");
+                }
+                counter++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ALLA PROGRAM SOM BÖRJAR KL 20.00 ");
+            Console.WriteLine();
+
+            foreach (Show show in allShows)
+            {
+                if (show.StartTime.Hours == 20)
+                {
+                    Console.WriteLine($"{show.Channel} {show.StartTime} {show.Title}");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ALLA PROGRAMNAME MED STORA BOKSTÄVER ");
+            Console.WriteLine();
+
+            foreach (Show show in allShows)
+            {
+                Console.WriteLine(show.Title.ToUpper());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ALLA KANALER ");
+            Console.WriteLine();
+
+            int i = 0;
+            foreach (Show show in allShows)
+            {
+                Console.WriteLine(show.Channel);
+                i++;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         private static double Sum(List<double> kalle1)
         {
@@ -72,28 +179,23 @@ namespace Module11
             }
             return totalen;
         }
-
         private static double Sum_WithLinq(List<double> listOfNumbers)
         {
             double sumTotalen = listOfNumbers.Sum();
 
             return sumTotalen;
         }
-
         private static double Average(List<double> listOfNumbers)
         {
             double averageResult = Sum(listOfNumbers)  / listOfNumbers.Count;
 
             return averageResult;
         }
-
         private static double Average_WithLinq(List<double> listOfNumbers)
         {
             double result = listOfNumbers.Average();
-
             return result;
         }
-
         private static List<double> NumbersHigherThanFive(List<double> listOfNumbers)
         {
             List<double> result = new List<double>();
@@ -107,7 +209,6 @@ namespace Module11
             }
             return result;
         }
-
         //private static StariFyList(List<double> output)
         //{
 
