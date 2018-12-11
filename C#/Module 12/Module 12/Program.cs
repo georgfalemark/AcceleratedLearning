811936d4d2fbf8b4fcda5e5a782cd365e18bf6ab
georgfalemark <georg@falemark.se>
2018-12-11 17:13:18 +0100

    module 11 och 12

---
+++ C#/Module 12/Module 12/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Module_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rockstarArray = CreateArrayOfRockstars();
+            List<string> rockstarList = CreateListOfRockstars();
+
+            DisplayArrayOfRockstars(rockstarArray);
+            DisplayListOfRockstars(rockstarList);
+
+            DisplayRockstars("My rockstars: (ienumerable)", rockstarArray);
+            DisplayRockstars("My rockstars: (ienumerable)", rockstarList);
+        }
+
+        private static string[] CreateArrayOfRockstars()
+        {
+            string[] rockstarsArray = new string[] { "Jim Morrison", "Ozzy Osborne", "Pelle Roses" };
+            return rockstarsArray;
+        }
+        private static List<string> CreateListOfRockstars()
+        {
+            List<string> rockstarList = new List<string> { "Jim Morrison", "Ozzy Osborne", "Pelle Roses" };
+            return rockstarList;
+        }
+
+        private static void DisplayArrayOfRockstars(string[] rockstarArray)
+        {
+            Console.WriteLine("My rockstars: (array)");
+            foreach (var item in rockstarArray)
+            {
+                Console.WriteLine($"* {item}");
+            }
+            Console.WriteLine();
+        }
+
+        private static void DisplayListOfRockstars(List<string> rockstarList)
+        {
+            Console.WriteLine("My rockstars: (list)");
+            foreach (var item in rockstarList)
+            {
+                Console.WriteLine($"* {item}");
+            }
+            Console.WriteLine();
+        }
+
+        private static void DisplayRockstars(string s, IEnumerable<string> rockstars)
+        {
+            Console.WriteLine("My rockstars: (ienumerable)");
+            foreach (var item in rockstars)
+            {
+                Console.WriteLine($"* {item}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
